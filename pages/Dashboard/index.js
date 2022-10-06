8b11e31b9d4c3abdf0ea6af6f89a93b7d6f6c477
AiniMalik <ainimalik9465@gmail.com>
2022-10-06 17:44:47 +0500

    dashboard header changes

--- pages/Dashboard/index.js
+++ pages/Dashboard/index.js
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import Sidebar from '../../components/Sidebar'
-import DashboardHeader from '../components/DashboardHeader';
+import DashboardHeader from '../../components/DashboardHeader';
 
 const Layout = ({ children }) => {
 
